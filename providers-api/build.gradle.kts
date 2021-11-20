47365d876fed6cd2cbf8075544f7918caf15eaf9
antonlapitskii <antlapit@gmail.com>
2021-11-20 22:40:23 +0300

    draft: Using github package registry

--- providers-api/build.gradle.kts
+++ providers-api/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
 
-    `maven-publish` apply false
+    `maven-publish`
 }
 
 repositories {
--- providers/build.gradle.kts
+++ providers/build.gradle.kts
@@ -12,6 +12,8 @@ plugins {
 
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+
+    `maven-publish`
 }
 
 repositories {
